192dd60ced0588f2ff5ea0028aa206866325baa2
Jiajie Chen <c@jia.je>
2022-04-12 23:16:27 +0800

    Implement GET_HARDWARE

--- jtagd.cpp
+++ jtagd.cpp
@@ -38,16 +38,29 @@ void add_int(uint32_t value) {
   send_buffer_size += sizeof(uint32_t);
 }
 
+int last_response = -1;
+
 // jtag_message.h RXMESSAGE::remove_response
 // 1-byte: resp
 // 1-byte: zero
 // 2-byte: length in big endian
-void add_response(uint8_t resp, uint16_t length) {
+void add_response(uint8_t resp) {
+  assert(last_response == -1);
   assert(send_buffer_size + 4 <= BUFFER_SIZE);
+  last_response = send_buffer_size;
   send_buffer[send_buffer_size++] = resp;
   send_buffer[send_buffer_size++] = 0;
-  send_buffer[send_buffer_size++] = (length >> 8);
-  send_buffer[send_buffer_size++] = length;
+  //  length will be filled below
+  send_buffer[send_buffer_size++] = 0;
+  send_buffer[send_buffer_size++] = 0;
+}
+
+void end_response() {
+  assert(last_response != -1);
+  uint16_t length = send_buffer_size - last_response;
+  send_buffer[last_response + 2] = length >> 8;
+  send_buffer[last_response + 3] = length;
+  last_response = -1;
 }
 
 uint16_t compute_header(uint16_t length, uint16_t mux) {
@@ -99,7 +112,11 @@ void jtag_jtagd_tick() {
       uint16_t mux = header >> 12;
       uint16_t length = (header & ((1 << 12) - 1)) + 1;
       if (buffer_begin + 2 + length <= buffer_end) {
-        dprintf("Received message of length %d\n", length);
+        dprintf("Received message of length %d:\n", length);
+        for (int i = 0; i < length; i++) {
+          dprintf("%02X ", (uint8_t)buffer[buffer_begin + 2 + i]);
+        }
+        dprintf("\n");
         buffer_begin += 2;
 
         // one or more messages
@@ -107,7 +124,44 @@ void jtag_jtagd_tick() {
         uint8_t *end = (uint8_t *)&buffer[buffer_begin + length];
         while (p < end) {
           MessageHeader *header = (MessageHeader *)p;
-          if (header->command == 0x83) {
+          if (header->command == 0x80) {
+            // GET_HARDWARE
+            dprintf("GET_HARDWARE\n");
+            // jtag_client_link.cpp AJI_CLIENT::get_hardware_from_server
+            // response:
+            add_response(0);
+            // an int: n: number of devices
+            int n = 1;
+            add_int(n);
+            // an int: fifo_len: payload size below
+            std::string hw_name = "hw0";
+            std::string port = "port0";
+            std::string device_name = "device0";
+            int fifo_len = 4 + 1 + hw_name.length() + 1 + port.length() + 4 +
+                           1 + device_name.length() + 4;
+            add_int(fifo_len);
+            end_response();
+            do_send(0);
+
+            // each payload:
+            // an int: chain_id
+            int chain_id = 0;
+            add_int(chain_id);
+            // a string: hw_name
+            add_string(hw_name);
+            // a string: port
+            add_string(port);
+            // an int: chain_type
+            int chain_type = 1; // JTAG
+            add_int(chain_type);
+            // a string: device_name
+            add_string(device_name);
+            // an int: features
+            int features = 1; // AJI_FEATURE_DUMMY
+            add_int(features);
+
+            do_send(4); // FIFO_MIN
+          } else if (header->command == 0x83) {
             // GET_VERSION_INFO
             dprintf("GET_VERSION_INFO\n");
             // response:
@@ -116,11 +170,11 @@ void jtag_jtagd_tick() {
             // a string: server path
             std::string version_info = "1.0";
             std::string server_path = "jtagd";
-            add_response(0, 4 + 1 + version_info.length() + 4 + 1 +
-                                server_path.length());
+            add_response(0);
             add_string(version_info);
             add_int(0);
             add_string(server_path);
+            end_response();
           } else if (header->command == 0xFE) {
             // USE_PROTOCOL_VERSION
             dprintf("USE_PROTOCOL_VERSION\n");
@@ -128,18 +182,22 @@ void jtag_jtagd_tick() {
             // response: flags
             int flags = 0;
             // 8: 4 header, 1 int
-            add_response(0, 8);
+            add_response(0);
             add_int(flags);
+            end_response();
           } else {
             dprintf("Unrecognized command: %x\n", header->command);
 
             // aji.h AJI_UNIMPLEMENTED
-            add_response(126, 4);
+            add_response(126);
+            end_response();
           }
           p += ntohs(header->be_len);
         }
 
-        do_send(0);
+        if (send_buffer_size > 2) {
+          do_send(0);
+        }
 
         buffer_begin += length;
       }
