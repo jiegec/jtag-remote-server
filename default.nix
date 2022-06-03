681489a8fc2f5029305ba698a8ce539a45e65621
Jiajie Chen <c@jia.je>
2022-06-03 13:10:15 +0800

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,17 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "jtag-remote-server";
+  version = "1.0";
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  buildInputs = [
+    libftdi1
+    pkgconfig
+  ];
+}
