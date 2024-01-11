1e733e253acd134973c0d18de7b53f17e471c663
Oliver Schwartz <schwartz@plexim.com>
2024-01-11 08:23:09 +0100

    Add missing includes.

--- src/mpsse_buffer.h
+++ src/mpsse_buffer.h
@@ -1,6 +1,9 @@
 #ifndef __MPSSE_BUFFER_H__
 #define __MPSSE_BUFFER_H__
 
+#include <stddef.h>
+#include <stdint.h>
+
 void mpsse_buffer_init(struct ftdi_context *ftdi);
 bool mpsse_buffer_ensure_space(size_t num_bytes);
 void mpsse_buffer_append_byte(uint8_t data);
